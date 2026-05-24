44a5c38901f4b83642cfdf47be6748b7bb9e7d0f
MatthewNguyen865 <matthewnguyen0629@gmail.com>
2026-05-23 22:52:32 -0500

    initialized nonlinear first order system

---
+++ models/first_order_system.py
@@ -0,0 +1,9 @@
+class FirstOrderSystem():
+    def __init__(self):
+        self.low_stable_equilibrium = 250
+        self.unstable_equilibrium = 300
+        self.high_stable_equilibrium = 350
+
+    def derivative(self, T):
+        dTdt = -(T - self.low_stable_equilibrium) * (T - self.unstable_equilibrium) * (T - self.high_stable_equilibrium)
+        return dTdt

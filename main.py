aeb0a498e0fd0bfc2c2d07336db92e7efa44bc64
MatthewNguyen865 <matthewnguyen0629@gmail.com>
2026-05-25 18:36:29 -0500

    Add nonlinear phase portrait visualization and flow analysis

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from experiments.basin_study import run_basin_study
 from visualization.plots import Plotting
+from models.first_order_system import FirstOrderSystem
 times, temperatures, labels =run_basin_study()
 Plotting.plot_trajectory(times, temperatures, labels)
+model = FirstOrderSystem()
+Plotting.plot_phase_portrait(model=model)
--- visualization/plots.py
+++ visualization/plots.py
@@ -29,3 +29,44 @@ class Plotting:
         plt.savefig(f"{PLOT_DIR}/temperature_trajectories.png")
 
         plt.show()
+    
+    
+    def plot_phase_portrait(model):
+        if not os.path.exists(PLOT_DIR):
+            os.makedirs(PLOT_DIR)
+        
+        T_values = np.linspace(220, 380, 100)
+        dTdt_values = [model.derivative(T) for T in T_values]
+        
+        plt.figure(figsize=(10, 6))
+        
+        #Plot dT/dt vs T and markers for equilibria
+        plt.plot(T_values, dTdt_values, label='dT/dt')
+        plt.scatter(model.low_stable_equilibrium, 0, marker='o', color='green', label='Stable Equilibria')
+        plt.scatter(model.unstable_equilibrium, 0, marker='o', edgecolors='red', facecolors='none', label='Unstable Equilibrium')
+        plt.scatter(model.high_stable_equilibrium, 0, marker='o', color='green')
+        
+        #add arrows to indicate direction of flow
+        low_midpoint = (model.low_stable_equilibrium+220)/2
+        plt.arrow(low_midpoint, 7, np.sign(model.derivative(low_midpoint))*10, 0, head_width=5, head_length=3, color='black')
+        
+        low_unstable_midpoint = (model.low_stable_equilibrium+model.unstable_equilibrium)/2
+        plt.arrow(low_unstable_midpoint, 7, np.sign(model.derivative(low_unstable_midpoint))*10, 0, head_width=5, head_length=3, color='black')
+        
+        high_unstable_midpoint = (model.high_stable_equilibrium+model.unstable_equilibrium)/2
+        plt.arrow(high_unstable_midpoint, 7, np.sign(model.derivative(high_unstable_midpoint))*10, 0, head_width=5, head_length=3, color='black')
+        
+        high_midpoint = (model.high_stable_equilibrium+380)/2
+        plt.arrow(high_midpoint, 7, np.sign(model.derivative(high_midpoint))*10, 0, head_width=5, head_length=3, color='black')
+        
+        plt.axhline(0, color='black', linestyle='--')
+        plt.xlabel('Temperature (T)')
+        plt.ylabel('Temperature rate of change (dT/dt)')
+        plt.title('Phase Portrait')
+        plt.grid()
+        
+        plt.legend()
+        plt.tight_layout()
+        plt.savefig(f"{PLOT_DIR}/phase_portrait.png")
+        
+        plt.show()

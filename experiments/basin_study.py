fb7150fdf8c56a0ccadb41b72b203e661451ef24
MatthewNguyen865 <matthewnguyen0629@gmail.com>
2026-05-25 08:47:50 -0500

    Add basin study experiment framework and structured trajectory visualization

---
+++ experiments/basin_study.py
@@ -0,0 +1,13 @@
+from models.first_order_system import FirstOrderSystem
+from simulation.simulator import EulerSimulator
+from parameters import initial_temps, time_step, num_steps
+def run_basin_study():
+    system = FirstOrderSystem()
+    simulator = EulerSimulator()
+    labels = []
+    all_trajectories = []
+    for T0 in initial_temps:
+        labels.append(f'T0={T0} K')
+        trajectory, times = simulator.simulate(system, initial_temp=T0, time_step=time_step, num_steps=num_steps)
+        all_trajectories.append(trajectory)
+    return times, all_trajectories, labels
